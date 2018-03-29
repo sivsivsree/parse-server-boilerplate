be44f24c6d8148860050b6c185b3a933fa897566
siv <siv@blockgemini.com>
2018-03-29 13:07:20 +0400

    Logger with starter

---
+++ index.js
@@ -0,0 +1,42 @@
+
+var express = require('express');
+var ParseServer = require('parse-server').ParseServer;
+var ParseDashboard = require('parse-dashboard');
+const PORT = process.env.PORT || 5000
+
+var app = express();
+
+var api = new ParseServer({
+    databaseURI: 'mongodb://tristar_dev:1234@ds227459.mlab.com:27459/tristar_android', // Connection string for your MongoDB database
+    appId: 'APPLICATION_ID',
+    masterKey: 'MASTER_KEY', // Keep this key secret!
+    fileKey: 'optionalFileKey',
+    serverURL: 'http://localhost:1337/parse', // Don't forget to change to https if needed 
+    liveQuery: {
+        classNames: ['FileUpload','GameScore']
+    }
+});
+
+var dashboard = new ParseDashboard({
+    "apps": [
+        {
+            "serverURL": "http://localhost:1337/parse",
+            "appId": "APPLICATION_ID",
+            "masterKey": "MASTER_KEY",
+            "appName": "Test Application",
+            "clientKey": "2ead5328dda34e688816040a0e78948a"
+        }
+    ]
+});
+
+// Serve the Parse API on the /parse URL prefix
+app.use('/parse', api);
+app.use('/dashboard', dashboard);
+
+var httpServer = require('http').createServer(app);
+httpServer.listen(4040);
+var parseLiveQueryServer = ParseServer.createLiveQueryServer(httpServer);
+
+app.listen(port, function() {
+    console.log('parse-server-example running on port 1337.');
+});
