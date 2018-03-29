da0b4b4a9ae4651119dc72f9a83d5374f25202bd
siv <siv@blockgemini.com>
2018-03-29 13:38:47 +0400

    Deployment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var ParseDashboard = require('parse-dashboard');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 // Serve the Parse API on the /parse URL prefix
